486a5edf44fb6629ae385b4a7abf0316c34724d3
dignego <vmendon@adobe.com>
2025-07-24 17:19:30 +0530

    Update sectionheader.js

--- blocks/sectionheader/sectionheader.js
+++ blocks/sectionheader/sectionheader.js
@@ -1,8 +1,8 @@
-export default function SectionHeader({ title }) {
+export default function SectionHeader({ text }) {
   return (
     <div className="section-header">
       <div className="section-header-line"></div>
-      <div className="section-header-title">{title}</div>
+      <div className="section-header-title">{text}</div>
       <div className="section-header-line"></div>
     </div>
   );
